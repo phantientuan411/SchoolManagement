7018e216c967590c9257cccf6b541b3cd553faac
toannguyen <toannguyen270999@gmail.com>
2025-11-10 20:00:26 +0700

    update router student

--- fe/src/router/AppRouter.tsx
+++ fe/src/router/AppRouter.tsx
@@ -1,14 +1,16 @@
-import {BrowserRouter as Router,  Route, Routes } from "react-router-dom";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import LoginInterface from "../features/login/loginInterface.tsx";
 import HomePage from "../features/homePage/HomePage.tsx";
+import ListStudentPage from "../features/listStudent/ListStudentPage";
 
 const AppRouter = () => {
   return (
     <Router>
-        <Routes>
-            <Route path="/" element={<LoginInterface />}/>
-            <Route path="/home" element={<HomePage />}/>
-        </Routes>
+      <Routes>
+        <Route path="/" element={<LoginInterface />} />
+        <Route path="/home" element={<HomePage />} />
+        <Route path="/student" element={<ListStudentPage></ListStudentPage>}></Route>
+      </Routes>
 
 
     </Router>
